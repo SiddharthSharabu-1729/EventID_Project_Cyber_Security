c8ed97375e4673d73a00460531d40130c7efa00f
SiddharthSharabu-1729 <siddharthsharanu123@gmail.com>
2022-01-24 21:06:24 +0530

    Edited Main File

--- main.py
+++ main.py
@@ -4,21 +4,45 @@ import subprocess
 import os
 from turtle import width
 
-from matplotlib.pyplot import text
+## The Main Root of this app
 
 root = tk.Tk()
 root.title("EventId")
+root.geometry("800x700")
+root.resizable(0,0)
 
-canva = tk.Canvas(root, height=600, width=700, bg="gray")
-canva.pack()
+## Creating a List Box to display the output
 
-frame = tk.Frame(root,bg="white")
-frame.place(relwidth="0.8",relheight="0.8",relx="0.01",rely="0.01")
+list = tk.Listbox(root, height=20, width=70, bg="#101010", font="Helvetica", fg="#AAFF00")
+#list.grid(row=0,rowspan=2,column=0,padx=10,pady=10)
+list.pack()
 
-frame1 = tk.Frame(root,bg="white")
-frame1.place(relwidth="0.98",relheight="0.8989",relx="0.01",rely="0.82")
+## Creating the frame for the RadioButtons
 
-run = tk.Button(root, text="Run",command="",bg="gray")
-run.pack()
+frame = tk.Frame(root, width=500, height=400, bd=1)
+
+var = tk.IntVar(value=0)
+
+def getc() :
+    x = var.get()
+    if x == 1 :
+        return "cmd"
+    elif x == 2 :
+        return "powershell"
+
+
+def com(comm) :
+    type = getc()
+    process=subprocess.Popen([type,comm],stdout=subprocess.PIPE)
+    result=process.communicate()[0].splitlines()
+    for i,j in enumerate(result) :
+        x = j.decode("utf-8")
+        list.insert(i, j)
+
+
+frame1 = tk.Frame(root)
+frame1.pack()
+x = tk.Radiobutton(frame1, text="CMD", variable=var, value=1, command=getc).pack(side="left")
+y = tk.Radiobutton(frame1, text="Powershell", variable=var, value=2, command=getc).pack(side="left")
 
 root.mainloop()

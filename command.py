6bc59e98579c3e9c1850123390d80f378a856a70
Siddharth_Sharabu <84086761+SiddharthSharabu-1729@users.noreply.github.com>
2022-01-26 21:09:07 +0530

    Delete command.py

--- command.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-import subprocess
-
-process=subprocess.Popen(["powershell","Get-EventLog -list"],stdout=subprocess.PIPE);
-result=process.communicate()[0].splitlines()
-
-for r in result :
-    x = r.decode("utf-8")
